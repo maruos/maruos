ed080412c847ea308df3d57e06505b9483872bd6
Preetam D'Souza <preetamjdsouza@gmail.com>
2020-05-09 15:29:11 -0400

    build(deps): Upgrade Vuepress from 0.x to 1.x
    This fixes a bunch of npm audit issues.
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,47 @@
 module.exports = {
-  title: 'Maru Docs',
-  description: 'Maru Documentation',
-  base: '/docs/',
-  ga: 'UA-72109002-1',
+  title: "Maru Docs",
+  description: "Maru Documentation",
+  base: "/docs/",
+  plugins: [
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-72109002-1",
+      },
+    ],
+  ],
   themeConfig: {
-    repo: 'maruos/maruos',
-    docsDir: 'docs',
+    repo: "maruos/maruos",
+    docsDir: "docs",
     editLinks: true,
     nav: [
-      { text: 'User Guide', link: '/user/' },
-      { text: 'Developer Guide', link: '/developer/' },
-      { text: 'Devices', link: '/devices/' },
-      { text: 'FAQ', link: '/faq/' },
-      { text: 'Release Notes', link: '/release-notes/' }
+      { text: "User Guide", link: "/user/" },
+      { text: "Developer Guide", link: "/developer/" },
+      { text: "Devices", link: "/devices/" },
+      { text: "FAQ", link: "/faq/" },
+      { text: "Release Notes", link: "/release-notes/" },
     ],
     sidebar: {
-      '/user/': [
+      "/user/": [
         {
-          title: 'User Guide',
+          title: "User Guide",
           collapsable: false,
           children: [
-            '',
-            '/user/installation',
-            '/user/upgrading',
-            '/user/google-apps',
-            '/user/tips',
-          ]
-        }
+            "",
+            "/user/installation",
+            "/user/upgrading",
+            "/user/google-apps",
+            "/user/tips",
+          ],
+        },
       ],
-      '/developer/': [
+      "/developer/": [
         {
-          title: 'Developer Guide',
+          title: "Developer Guide",
           collapsable: false,
-          children: [
-            '',
-            'env',
-            'download',
-            'build',
-            'porting'
-          ]
-        }
-      ]
-    }
-  }
-}
+          children: ["", "env", "download", "build", "porting"],
+        },
+      ],
+    },
+  },
+};
